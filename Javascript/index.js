a1eeebd1503db5bee0e38e7b0ae1d09ffff3e79b
nariko1228 <nariko1228@live.com>
2024-05-21 21:50:45 +0100

    Update index.js

--- Javascript/index.js
+++ Javascript/index.js
@@ -44,7 +44,7 @@ function updateCity(event) {
           <div class="time">${cityTime.format(
             "h:mm:ss"
           )}<small>${cityTime.format("A")}</small></div>
-        </div>
+        </div></br>
         <a href="/">All cities</a>
         `;
 }
